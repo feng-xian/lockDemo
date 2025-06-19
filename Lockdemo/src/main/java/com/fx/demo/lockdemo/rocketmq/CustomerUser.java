2051801d2f5a03e537ebb7f4b0f85252dfc7f8b7
凤仙 <2218050479@qq.com>
2025-06-19 21:00:57 +0800

    移除mq依赖和配置

--- Lockdemo/src/main/java/com/fx/demo/lockdemo/rocketmq/CustomerUser.java
+++ Lockdemo/src/main/java/com/fx/demo/lockdemo/rocketmq/CustomerUser.java
@@ -1,31 +1,31 @@
-package com.fx.demo.lockdemo.rocketmq;
-
-import com.fx.demo.lockdemo.entity.User;
-import com.fx.demo.lockdemo.utils.JacksonUtils;
-import org.apache.rocketmq.spring.annotation.ConsumeMode;
-import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
-import org.apache.rocketmq.spring.core.RocketMQListener;
-import org.springframework.stereotype.Component;
-
-@Component
-@RocketMQMessageListener(
-        consumerGroup = "SYNC-USER-GROUP"
-        , topic = "SYNC-USER"
-        , consumeMode = ConsumeMode.ORDERLY
-        , consumeThreadMax = 4 // 设置最大线程数为4
-)
-public class CustomerUser  implements RocketMQListener<String> {
-
-    @Override
-    public void onMessage(String s) {
-        User user = null;
-        try {
-            user = JacksonUtils.json2pojo(s, User.class);
-        } catch (Exception e) {
-            System.out.println("ThreadId=" + Thread.currentThread().getId() + "error");
-            throw new RuntimeException(e);
-        }
-        System.out.println("ThreadId=" + Thread.currentThread().getId() + ": 收到消息：" + user);
-    }
-
-}
+//package com.fx.demo.lockdemo.rocketmq;
+//
+//import com.fx.demo.lockdemo.entity.User;
+//import com.fx.demo.lockdemo.utils.JacksonUtils;
+//import org.apache.rocketmq.spring.annotation.ConsumeMode;
+//import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+//import org.apache.rocketmq.spring.core.RocketMQListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RocketMQMessageListener(
+//        consumerGroup = "SYNC-USER-GROUP"
+//        , topic = "SYNC-USER"
+//        , consumeMode = ConsumeMode.ORDERLY
+//        , consumeThreadMax = 4 // 设置最大线程数为4
+//)
+//public class CustomerUser  implements RocketMQListener<String> {
+//
+//    @Override
+//    public void onMessage(String s) {
+//        User user = null;
+//        try {
+//            user = JacksonUtils.json2pojo(s, User.class);
+//        } catch (Exception e) {
+//            System.out.println("ThreadId=" + Thread.currentThread().getId() + "error");
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println("ThreadId=" + Thread.currentThread().getId() + ": 收到消息：" + user);
+//    }
+//
+//}
